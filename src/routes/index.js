77c798ba19c373acaa34e41ab5746fb3143518a6
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2024-07-17 17:36:56 -0300

    Ajuste das importações
    Eslint configurado para aceitar ES modules, e importações ajustadas.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
-import { Router } from 'express';
-var router = Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
