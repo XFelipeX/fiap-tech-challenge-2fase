9f9bc06da7fd2cd6f35c116bfd3a027265e75df9
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2024-07-25 20:28:46 -0300

    Diretório de produção alterado para "dist"

--- build/entities/teacher.entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Teacher = void 0;
-class Teacher {
-    constructor(name) {
-        this.name = name;
-    }
-}
-exports.Teacher = Teacher;
